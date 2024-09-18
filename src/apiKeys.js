663f0f440c356c6edeb63c2b9be7d18a7db8069c
Vinod Phalke <vinodcphalke@gmail.com>
2024-09-18 15:47:20 +0530

    source

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-    key: "ee77d37fa4c63a0e3619356395c61ccd",
-    base: "https://api.openweathermap.org/data/2.5/",
-  };
+  key: "ee77d37fa4c63a0e3619356395c61ccd",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
